159528e0fd909c112916370f5320aa714ecb2755
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-14 21:26:44 +0100

    add dictionaries.py

---
+++ lecture2/dictionaries.py
@@ -0,0 +1,11 @@
+# In dictionaries when we search for "KEYS" we receive the "VALUE"
+
+races = {"Aragorn": "Human", "Frodo": "Hobbit"}
+
+print(races["Frodo"])
+
+# It is possible to add KEYS with their VALUES later in the code
+
+races["Gimli"] = "Dwarf"
+
+print(races["Gimli"])
