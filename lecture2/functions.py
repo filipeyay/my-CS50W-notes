762490b8e5595972952c49ebc2adac66aa1bf977
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-14 21:30:07 +0100

    add functions.py

---
+++ lecture2/functions.py
@@ -0,0 +1,11 @@
+# NOTE: def = define
+
+
+def square(x):
+    return x * x
+
+
+# NOTE: code below commented so it could be imported on 'squares.py'
+
+# for i in range(10):
+#    print(f"The square of {i} it's {square(i)}.")
