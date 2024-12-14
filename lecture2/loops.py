497ed87fa79e6fdc09e544dd92ae903a8c1c40d1
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-14 21:37:01 +0100

    add loops.py

---
+++ lecture2/loops.py
@@ -0,0 +1,23 @@
+# An example of a simple loop would be
+
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+# Another way of doing it with less code would be
+
+for y in range(51):
+    print(y)
+
+# It could also be:
+
+names = ["Aragorn", "Frodo", "Sam", "Gandalf"]
+
+for name in names:
+    print(name)
+
+# And
+
+villain = "Gollum"
+
+for characters in villain:
+    print(characters)
