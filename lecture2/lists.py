1cf95144b367a1a73244cc8d09b25eec5cfebc0b
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-14 21:33:56 +0100

    add lists.py

---
+++ lecture2/lists.py
@@ -0,0 +1,11 @@
+names = ["Aragorn", "Sam", "Frodo", "Gandalf"]
+
+# Add one more name to the previous list, this is possible because it is 'changeable'
+
+names.append("Gollum")
+
+# Sort the list, in this case in alphabetical order
+
+names.sort()
+
+print(names)
