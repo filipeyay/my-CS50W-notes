c59e56c62cc3b6f6b41e908360f003cb282de77c
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-14 21:24:26 +0100

    add sequences.py

---
+++ lecture2/sequences.py
@@ -0,0 +1,18 @@
+name = "Nikdra"
+
+# In square brackets, right after the variable, you can insert the number you want to be displayed. It will always start at 0. In this case, the character 0 is N
+
+print(name[0])
+
+hobbits = ["Bilbo", "Frodo", "Sam"]
+
+# In another example we will see a list, and in this case the item shown will not be a character but rather the second item in the list
+
+print(hobbits[1])
+
+# This example shows 2 ways of demonstrating the same thing, but the first is traditional and repetitive and the second is a better way
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinates = (10.0, 20.0)
