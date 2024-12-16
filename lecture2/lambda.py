ee2513019d9599aec2fc8375886a82ebf3a89962
Filipe Andrade <94734826+filipeyay@users.noreply.github.com>
2024-12-16 16:36:40 +0100

    add lambda.py

---
+++ lecture2/lambda.py
@@ -0,0 +1,11 @@
+# lambda -> is a way to create an anonymous function, that is, a function without a defined name, quickly and simply
+
+people = [
+    {"name": "Aragorn", "race": "Human"},
+    {"name": "Frodo", "race": "Hobbit"},
+    {"name": "Legolas", "race": "Elfo"},
+]
+
+# the lambda function will get the 'people' input and will return the output 'people["<key>"]'
+
+print(people)
